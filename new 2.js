ffe9b88a5ed08183a6bcfe856a33aad1f49bdde7
polchik020805-ctrl <polchik020805@gmail.com>
2026-04-30 15:17:02 +0500

    Add files via upload

--- new 2.js
+++ new 2.js
@@ -1,120 +1,30 @@
-import * as THREE from "three";
-import { OrbitControls } from "jsm/controls/OrbitControls.js";
-import { HDRLoader } from "jsm/loaders/HDRLoader.js";
+const IMAGE_SRCS = [
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000);
-const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000,
-);
-camera.position.z = 5;
+  "https://raw.githack.com/polchik020805-ctrl/20.03/refs/heads/main/sliva.jpeg",
 
-const renderer = new THREE.WebGLRenderer({
-	canvas: document.querySelector(".webgl"),
-	antialias: true,
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
+  "https://raw.githack.com/polchik020805-ctrl/20.03/refs/heads/main/persik.jpeg",
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
+  "https://raw.githack.com/polchik020805-ctrl/20.03/refs/heads/main/grusha.jpeg",
 
-const textureLoader = new THREE.TextureLoader();
+  "https://raw.githack.com/polchik020805-ctrl/20.03/refs/heads/main/baf08ff447411f1a4af3668936626c3_1.jpeg",
 
-const particlesGroup = new THREE.Group();
-scene.add(particlesGroup);
+  "https://raw.githack.com/polchik020805-ctrl/20.03/refs/heads/main/Banan.jpeg",
 
-const groupCount = 3;
-const particlesPerGroup = 30;
+  "https://raw.githack.com/polchik020805-ctrl/20.03/refs/heads/main/klubnika.jpeg"
 
-const palette = [
-	new THREE.Color(0x00ffcc),
-	new THREE.Color(0x00e5ff),
-	new THREE.Color(0xffffff),
 ];
-for (let g = 0; g < groupCount; g++) {
-	const video = document.createElement("video");
-	video.crossOrigin = "anonymous";
-	video.src =
-		"https://raw.githubusercontent.com/polchik020805/20.03/main/5e67d02a0164aac690e9b7570dafacd2-Picsart-BackgroundRemover.webm";
-	video.loop = true;
-	video.muted = true;
-	video.playbackRate = 0.010 + Math.random() * 1.0;
-	video.play();
-	const videoTexture = new THREE.VideoTexture(video);
-	videoTexture.colorSpace = THREE.SRGBColorSpace;
-	videoTexture.minFilter = THREE.LinearFilter;
-	videoTexture.magFilter = THREE.LinearFilter;
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
 
-	const particlesGeometry = new THREE.BufferGeometry();
-	const positionsArray = new Float32Array(particlesPerGroup * 3);
-	const colorsArray = new Float32Array(particlesPerGroup * 3);
+  "Sliva",
 
-	for (let i = 0; i < particlesPerGroup; i++) {
-		const i3 = i * 3;
+  "Persik",
 
-		positionsArray[i3] = (Math.random() - 0.5) * 30;
-		positionsArray[i3 + 1] = (Math.random() - 0.5) * 30;
-		positionsArray[i3 + 2] = (Math.random() - 0.5) * 30;
+  "Grusha",
 
-		const randomColor = palette[Math.floor(Math.random() * palette.length)];
-		colorsArray[i3] = randomColor.r;
-		colorsArray[i3 + 1] = randomColor.g;
-		colorsArray[i3 + 2] = randomColor.b;
-	}
+  "Yabloko",
 
-	particlesGeometry.setAttribute(
-		"position",
-		new THREE.BufferAttribute(positionsArray, 3),
-	);
-	particlesGeometry.setAttribute(
-		"color",
-		new THREE.BufferAttribute(colorsArray, 3),
-	);
-  
-	const particlesMaterial = new THREE.PointsMaterial({
-		size: 10,
-		transparent: true,
-		depthWrite: false,
-		alphaTest: 0.001,
-		alphaMap: videoTexture,
-		vertexColors: false,
-	});
+  "Banan",
 
-	const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-	particles.frustumCulled = false;
-
-	particlesGroup.add(particles);
-}
-const hdrLoader = new HDRLoader();
-const hdrTexture = hdrLoader.load(
-	"https://raw.githubusercontent.com/polchik020805/20.03/refs/heads/main/little_paris_eiffel_tower_1k.hdr",
-	() => {
-		hdrTexture.mapping = THREE.EquirectangularReflectionMapping;
-		hdrTexture.colorSpace = THREE.SRGBColorSpace;
-		scene.background = hdrTexture;
-		scene.backgroundRotation.y = (Math.PI / 180) * -160;
-		scene.backgroundRotation.x = (Math.PI / 180) * -20;
-	},
-);
-
-const clock = new THREE.Clock();
-
-const tick = () => {
-	const elapsedTime = clock.getElapsedTime();
-
-	particlesGroup.rotation.x = elapsedTime * 0.1;
-
-	controls.update();
-	renderer.render(scene, camera);
-	requestAnimationFrame(tick);
-};
-tick();
-
-window.addEventListener("resize", () => {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-});
+  "Klubnika"
+];
